87451806134ebd01e07d40f7a47b54c678d8e627
Tenebrae <80919466+ZodiackiIler@users.noreply.github.com>
2024-03-10 20:33:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -18,6 +18,12 @@ LOGS_CHAT_ID = os.getenv("LOGS_CHAT_ID")
 
 SQLITE3_CONNECT = os.getenv("SQLITE3_CONNECT")
 
+MYSQL_ADDRESS = os.getenv("MYSQL_ADDRESS")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+
 DATABASE_TYPE = ('sqlite3')
 
 start_message_for_user = f"""
@@ -30,6 +36,12 @@ start_message_for_admin = f"""
 <b>Приветствую тебя, дорогой администратор!</b>
 Я - Бот для активации промокодов, и предназначен для оптимизации выдаче призов с помощью промокодов.
 Если ты хочешь создать новый промокод, то можешь воспользоваться командой, чтобы создать его <code>/create `promocode` `balance` `count` `description`</code>, причем `description` может быть пустым.
+"""
+info_commands_admin = f"""
+<b>Список команд для администратора:</b>
+<code>/create `promocode` `balance` `count` `description`</code> - создать промокод
+<code>/stats</code> - статистика по промокодам
+<code>/info</code> - информация о боте
 """
 
 # Объект бота
