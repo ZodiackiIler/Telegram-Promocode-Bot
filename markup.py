272447c384d0666d4f45101d0f3f3733f9847fcd
Tenebrae <80919466+ZodiackiIler@users.noreply.github.com>
2024-03-10 20:35:01 +0300

    Update markup.py

--- markup.py
+++ markup.py
@@ -4,5 +4,5 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeybo
 admin_menu = InlineKeyboardMarkup(resize_keyboard=True)
 statsbtn = InlineKeyboardButton("Статистика", callback_data="statsbtn")
 infobtn = InlineKeyboardButton("Информация", callback_data="infobtn")
-logs_linkbtn = InlineKeyboardButton("Чат с логами", url="YOU_CHAT_LOG_ID")
+logs_linkbtn = InlineKeyboardButton("Чат с логами", url="YOU_CHAT_LOG_LINK")
 admin_menu.row(statsbtn, infobtn).add(logs_linkbtn)
